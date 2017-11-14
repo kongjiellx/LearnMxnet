d0f6fc758310e77dcfecce1cd5d5e6ddb05e8faf
kongjiellx <623239411@qq.com>
2017-11-15 01:08:32 +0800

    add test

--- code/test.py
+++ code/test.py
@@ -3,6 +3,7 @@
 
 from model import get_net
 import mxnet as mx
+from mxnet.gluon.data import vision
 from setting import *
 from load_data import load_data, transform_train, transform_test
 from util import utils
@@ -18,12 +19,13 @@ if __name__ == "__main__":
     for data, label in test_data:
         output = net(data.as_in_context(ctx))
         preds.extend(output.argmax(axis=1).astype(int).asnumpy())
+        break
 
     sorted_ids = list(range(1, 300000 + 1))
     sorted_ids.sort(key = lambda x:str(x))
 
-    train_valid_ds = vision.ImageFolderDataset(input_str + 'train_valid',
+    train_ds = vision.ImageFolderDataset(input_str + 'train',
                                            flag=1, transform=transform_train)
     df = pd.DataFrame({'id': sorted_ids, 'label': preds})
-    df['label'] = df['label'].apply(lambda x: train_valid_ds.synsets[x])
+    df['label'] = df['label'].apply(lambda x: train_ds.synsets[x])
     df.to_csv('submission.csv', index=False)
