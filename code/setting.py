6cab5d6eb790276ac9fce73bd99e5ea072e44708
kongjiellx <623239411@qq.com>
2017-11-16 00:39:45 +0800

    revert

--- code/setting.py
+++ code/setting.py
@@ -6,8 +6,8 @@ test_dir = 'test_tiny'
 input_str = data_dir + input_dir + '/'
 model_path = '../model/cifar10.params'
 
-batch_size = 128
-num_epochs = 50
+batch_size = 2048
+num_epochs = 100
 learning_rate = 0.1
 num_outputs = 10
 
