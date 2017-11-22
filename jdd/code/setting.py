ab5c1986702ed1a6bfdc20bb9c5d1db173df0f42
kongjiellx <623239411@qq.com>
2017-11-23 01:25:06 +0800

    80

--- jdd/code/setting.py
+++ jdd/code/setting.py
@@ -2,11 +2,11 @@ data_dir = '../data/'
 model_path = '../model/pig.params'
 
 batch_size = 128
-num_epochs = 220
+num_epochs = 80
 learning_rate = 0.1
 num_outputs = 30
 
-lr_period = 50
+lr_period = 24
 lr_decay = 0.2
 weight_decay = 1e-4
 
