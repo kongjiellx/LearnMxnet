a7621ea9d97a233b097edaa6aa09875f9e3b6535
kongjiellx <623239411@qq.com>
2017-11-22 23:41:52 +0800

    a

--- jdd/code/setting.py
+++ jdd/code/setting.py
@@ -1,9 +1,4 @@
 data_dir = '../data/'
-label_file = 'trainLabels.csv'
-input_dir = 'train_valid_test'
-train_dir = 'train_tiny'
-test_dir = 'test_tiny'
-input_str = data_dir + input_dir + '/'
 model_path = '../model/pig.params'
 
 batch_size = 128
