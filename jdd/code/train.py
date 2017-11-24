0d1f08e30a744d39b1a18bb400b998329d99e950
kongjiellx <623239411@qq.com>
2017-11-24 22:42:54 +0800

    collect params

--- jdd/code/train.py
+++ jdd/code/train.py
@@ -51,18 +51,17 @@ def train(net, train_data, valid_data, num_epochs, lr, wd, ctx, lr_period, lr_de
                             train_acc / len(train_data)))
         prev_time = cur_time
         print(epoch_str + time_str + ', lr ' + str(trainer.learning_rate))
-        net.save_params(model_path)
+        net.collect_params.save(model_path)
 
 if __name__ == '__main__':
     train_data = load_data(data_dir + 'train', transform_train, True)
 
     ctx = utils.try_gpu()
-    pretrained_net = models.resnet50_v2(pretrained=True, ctx=ctx)
-    net = models.resnet50_v2(classes=num_outputs, ctx=ctx)
+    pretrained_net = models.resnet50_v2(pretrained=True)
+    net = models.resnet50_v2(classes=num_outputs)
     net.features = pretrained_net.features
-    net.output.initialize(ctx=ctx, init=init.Xavier())
+    net.output.initialize(init=init.Xavier())
     train(net, train_data, None, num_epochs, learning_rate,
             weight_decay, ctx, lr_period, lr_decay)
-    net.collect_params.save(model_path)
 
 
